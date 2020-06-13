ec0e8e59ae85f25c99336378d4f6264dd56797e3
goodmanager <tian0wen12@126.com>
2020-06-13 14:10:51 +0800

    修改注册中心策略

--- probe-rpc-framework-common/src/main/java/com/felix/rpc/framework/common/config/RegisterCenterType.java
+++ probe-rpc-framework-common/src/main/java/com/felix/rpc/framework/common/config/RegisterCenterType.java
@@ -2,36 +2,35 @@ package com.felix.rpc.framework.common.config;
 
 /**
  * 注册中心类型
- * 
- * @author felix
  *
+ * @author felix
  */
 public enum RegisterCenterType {
 
-	ZOOKEEPER(1, "zookeeper"), CONSUL(2, "consul");
+    ZOOKEEPER(1, "zookeeper"), CONSUL(2, "consul"), NACOS(3, "nacos");
 
-	private int index;
+    private int index;
 
-	private String description;
+    private String description;
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public void setIndex(int index) {
-		this.index = index;
-	}
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	RegisterCenterType(int index, String description) {
-		this.index = index;
-		this.description = description;
-	}
+    RegisterCenterType(int index, String description) {
+        this.index = index;
+        this.description = description;
+    }
 }
--- probe-rpc-framework-common/src/main/java/com/felix/rpc/framework/common/config/SelectStrategy.java
+++ probe-rpc-framework-common/src/main/java/com/felix/rpc/framework/common/config/SelectStrategy.java
@@ -2,39 +2,38 @@ package com.felix.rpc.framework.common.config;
 
 /**
  * 负载均衡策略
- * 
- * @author felix
  *
+ * @author felix
  */
 public enum SelectStrategy {
 
-	RANDOM(1, "Random"), // 随机
-	ROUNDROBIN(2, "RoundRobin"), // 轮训
-	BESTAVAILABLE(3, "BestAvailable"), // 并发数低
-	WEIGHTEDRESPONSETIME(4, "WeightedResponseTime");// 加权
+    RANDOM(1, "Random"), // 随机
+    ROUNDROBIN(2, "RoundRobin"), // 轮训
+    BESTAVAILABLE(3, "BestAvailable"), // 并发数低
+    WEIGHTEDRESPONSETIME(4, "WeightedResponseTime");// 加权
 
-	private int index;
-	private String description;
+    private int index;
+    private String description;
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public void setIndex(int index) {
-		this.index = index;
-	}
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	SelectStrategy(int index, String description) {
-		this.index = index;
-		this.description = description;
-	}
+    SelectStrategy(int index, String description) {
+        this.index = index;
+        this.description = description;
+    }
 
 }
